22dee352270d3f3eb7d5c6197a32978e8947cfe4
meytalmesh <meytalmesh@gmail.com>
2021-02-10 13:21:13 +0200

    a star

--- src/Problem/IGame.java
+++ src/Problem/IGame.java
@@ -10,5 +10,6 @@ public interface IGame {
     int G();     //The distance between the start vertex and the vertex 𝑥
     double F();  //G+H
     IGame getPrev();
+    void setG(int g);
 
 }
--- src/SearchAlgorithms/AStar.java
+++ src/SearchAlgorithms/AStar.java
@@ -12,7 +12,7 @@ public class AStar implements ProblemSolver{
     private PriorityQueue<IGame> open_list;
     HashMap<IGame, Double> open_list_hash;
     HashSet<IGame> closed_list_hash;
-
+    private int g=0;
     @Override
     public void solvePuzzle(IGame game) {
 
@@ -31,6 +31,50 @@ public class AStar implements ProblemSolver{
         open_list.add(game);
         IGame goal = null;
         long end = 0;
+        while (!open_list.isEmpty()){
+            IGame current = open_list.poll();
+            if(current.isGoal()){
+                goal = current;
+                end = System.currentTimeMillis();
+                System.out.println("Goal found!");
+                break;
+            }
+            if(closed_list_hash.contains(current)){
+                System.exit(-1);
+            }
+            for (IGame neighbor : current.getNeighbors()) {
+                neighbor.setG(g);
+                double neighbor_cost = neighbor.F();
+                if(closed_list_hash.contains(neighbor)){
+                    continue;
+                }
+                if(!open_list_hash.containsKey(neighbor)){
+                    open_list_hash.put(neighbor, neighbor.F());
+                    open_list.add(neighbor);
+                }else{
+                    double old_f = open_list_hash.get(neighbor);
+                    if(old_f <= neighbor_cost){
+                        continue;
+                    }else{
+                        open_list_hash.remove(neighbor);
+                        open_list.remove(neighbor);
+                        open_list.add(neighbor);
+                        open_list_hash.put(neighbor, neighbor.F());
+
+                    }
+                }
+            }
+            closed_list_hash.add(current);
+            g++;
+        }
+        int cost = 0;
+        cost= (int) goal.F();
+
+        float sec = (end - start) / 1000F;
+        System.out.println("Time To Solve: "+sec + " seconds");
+        System.out.println("Solution cost: "+cost);
+        System.out.println("Open size: "+ open_list.size());
+        System.out.println("Expended: "+ closed_list_hash.size());
 
 
 
